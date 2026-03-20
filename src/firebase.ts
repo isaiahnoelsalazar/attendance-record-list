9883864caad6d9c6a75cbf29d4439e86b95b037e
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-03-20 12:09:32 +0800

    change config key call

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,8 +6,8 @@ import { getStorage, ref, uploadString, getDownloadURL } from 'firebase/storage'
 const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
